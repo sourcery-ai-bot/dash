9ca798485c343446fa159a2d1e68b50d70368d4a
Dave Glowacki <dglo@icecube.wisc.edu>
2006-12-19 16:47:09 +0000

    Use '-ww' (wide-wide) option so 'ps' output isn't truncated

--- lhlaunch.sh
+++ lhlaunch.sh
@@ -8,13 +8,13 @@ echo "Killing existing components..."
 ./CnCServer.py -k
 ./DAQRun.py    -k 
 
-ps ax | egrep 'java icecube.daq.juggler.toybox.DAQCompApp' | grep -v grep | awk '{print $1}' | xargs kill -9
-ps ax | egrep 'java icecube.daq.eventBuilder.EBComponent'  | grep -v grep | awk '{print $1}' | xargs kill -9
-ps ax | egrep 'java icecube.daq.trigger.component.IniceTriggerComponent'\
+ps axww | egrep 'java icecube.daq.juggler.toybox.DAQCompApp' | grep -v grep | awk '{print $1}' | xargs kill -9
+ps axww | egrep 'java icecube.daq.eventBuilder.EBComponent'  | grep -v grep | awk '{print $1}' | xargs kill -9
+ps axww | egrep 'java icecube.daq.trigger.component.IniceTriggerComponent'\
                                                            | grep -v grep | awk '{print $1}' | xargs kill -9
-ps ax | egrep 'java icecube.daq.trigger.component.GlobalTriggerComponent'\
+ps axww | egrep 'java icecube.daq.trigger.component.GlobalTriggerComponent'\
                                                            | grep -v grep | awk '{print $1}' | xargs kill -9
-ps ax | egrep 'java -Dicecube.daq.stringhub'               | grep -v grep | awk '{print $1}' | xargs kill -9
+ps axww | egrep 'java -Dicecube.daq.stringhub'               | grep -v grep | awk '{print $1}' | xargs kill -9
 
 echo "Cleaning up logs..."
 
