6afa682588309ee97d826d35696e6b2f41442b86
Kael Hanson <kael@icecube.wisc.edu>
2007-01-03 17:37:37 +0000

    make more silent ala '@'-prefix

--- launchnt.bat
+++ launchnt.bat
@@ -1,21 +1,21 @@
 @echo Starting a DAQ run under Windows NT
 @SET CONFIGDIR=..\config
-@SET LOGADDR=saxony:9001
+@SET LOGADDR=localhost:9001
 
 @echo Starting Pythonic glue processes ...
-start DAQRun.py -c %CONFIGDIR% -l ..\log -n
-start CnCServer.py -l %LOGADDR%
+@start "DAQ-RUN" DAQRun.py -c %CONFIGDIR% -l ..\log -n
+@start "CNC-SERVER" CnCServer.py -l %LOGADDR%
 
 @echo Bringing up the DAQ components ...
 
 @echo #1 - EventBuilder ...
-start java icecube.daq.eventBuilder.EBComponent -g %CONFIGDIR% -l %LOGADDR%
-sleep 1
+@start "EVENT-BUILDER" java icecube.daq.eventBuilder.EBComponent -g %CONFIGDIR% -l %LOGADDR%
+@sleep 1
 
 @echo #2 - Triggers
-start java icecube.daq.trigger.component.GlobalTriggerComponent -g %CONFIGDIR% -l %LOGADDR%
-start java icecube.daq.trigger.component.IniceTriggerComponent -g %CONFIGDIR% -l %LOGADDR%
-sleep 1
+@start "GLOBAL TRIGGER" java icecube.daq.trigger.component.GlobalTriggerComponent -g %CONFIGDIR% -l %LOGADDR%
+@start "INICE TRIGGER" java icecube.daq.trigger.component.IniceTriggerComponent -g %CONFIGDIR% -l %LOGADDR%
+@sleep 1
 
 @echo #3 - StringHub
-start java -Dicecube.daq.stringhub.componentId=1001 icecube.daq.stringhub.Shell -g %CONFIGDIR% -l %LOGADDR%
+@start "STRINGHUB-1001" java -Dicecube.daq.stringhub.simulation=true -Dicecube.daq.stringhub.componentId=1001 icecube.daq.stringhub.Shell -g %CONFIGDIR% -l %LOGADDR%
