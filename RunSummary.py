0b565e5a3e235f2fb16d410e43a9d997d13615b1
John Jacobsen <john@mail.npxdesigns.com>
2007-01-11 16:42:16 +0000

    Create daqruns summary website for NH (or NPX?) viewing of runs

--- RunSummary.py
+++ RunSummary.py
@@ -38,30 +38,13 @@ if __name__ == "__main__":
     runDir = opt.outputDir+"/runs"
     check_make_or_exit(runDir)
 
-
-    # Find all SPADE files in the form:
-    # SPS-pDAQ-run-001_20070108_174324_000015.dat.tar
-
-    def makeSnippet(snippetFile, infoPat, runInfo, runLink, configName, status):
-        match = search(infoPat, runInfo)
-        if not match: return
-        runNum = int(match.group(1))
-        year   = int(match.group(2))
-        month  = int(match.group(3))
-        day    = int(match.group(4))
-        hr     = int(match.group(5))
-        mins   = int(match.group(6))
-        sec    = int(match.group(7))
-        dur    = int(match.group(8))
+    def generateSnippet(snippetFile, runNum, month, day, year, hr, mins, sec, dur,
+                        configName, runDir, status):
+        
         snippet = open(snippetFile, 'w')
 
-        # Calculate status color
-        statusColor = "EFEFEF"
-        if status == "FAIL":
-            statusColor = "FF3300"
-        elif status == "SUCCESS":
-            statusColor = "CCFFCC"
-            
+        statusColor = getStatusColor(status)
+        
         print >>snippet, """
         <tr>
         <td align=center>%d</td>
@@ -72,7 +55,95 @@ if __name__ == "__main__":
         <td align=center bgcolor=%s><a href="%s">%s</a></td>
         </tr>
         """ % (runNum, month, day, year, hr, mins, sec, dur,
-               configName, statusColor, runLink, status)
+               configName, statusColor, runDir, status)
+        return
+
+    def getStatusColor(status):
+        # Calculate status color
+        statusColor = "EFEFEF"
+        if status == "FAIL":
+            statusColor = "FF3300"
+        elif status == "SUCCESS":
+            statusColor = "CCFFCC"
+        return statusColor
+
+    def makeTable(files, name):
+        html = ""
+        if files:
+            html += "<PRE>\n\n</PRE><TABLE>"
+            virgin = True
+            for l in files:
+                html += "<TR>"
+                if virgin: html += r'<TD ALIGN="right"><FONT COLOR=888888>%s</FONT></TD>' % name
+                else: html += "<TD></TD>"
+                virgin = False
+                html += r'<TD><A HREF="%s">%s</A></TD>' % (l, l)
+                html += "</TR>"
+            html += "</TABLE>"
+        return html
+
+    def makeSummaryHtml(logLink, runNum, month, day, year, hr,
+                        mins, sec, dur, configName, status):
+        files = listdir(logLink)
+        mons  = []
+        logs  = []
+        for f in files:
+            if search(r'\.log$', f): logs.append(f)
+            if search(r'\.moni$', f): mons.append(f)
+
+        html = open(logLink+"/run.html", "w")
+
+        print >>html, "<HTML>"
+        print >>html, "<TABLE><TR><TD BGCOLOR=EEEEEE>"
+        print >>html, """
+<TABLE>
+ <TR><TD ALIGN="right"><FONT COLOR=888888>Run</FONT></TD><TD><FONT SIZE=+3>%d</FONT></TD></TR>
+ <TR><TD ALIGN="right"><FONT COLOR=888888>Configuration</FONT></TD><TD>%s</TD></TR>
+ <TR><TD ALIGN="right"><FONT COLOR=888888>Date</FONT></TD><TD>%s</TD></TR>
+ <TR><TD ALIGN="right"><FONT COLOR=888888>Time</FONT></TD><TD>%s</TD></TR>
+ <TR><TD ALIGN="right"><FONT COLOR=888888>Duration</FONT></TD><TD>%d seconds</TD></TR>
+ <TR><TD ALIGN="right"><FONT COLOR=888888>Status</FONT></TD><TD BGCOLOR=%s>%s</TD></TR>
+</TABLE>
+        """ % (runNum,
+               configName,
+               "%02d/%02d/%02d" % (month, day, year),
+               "%02d:%02d:%02d" % (hr, mins, sec),
+               dur, 
+               getStatusColor(status), status)
+
+        print >>html, makeTable(logs, "Logs")
+        print >>html, makeTable(mons, "Monitoring")
+
+        print >>html, "</TD><TD>"
+        
+        dashlog = logLink+"/dash.log"
+        if exists(dashlog):
+            print >>html, "<PRE>"
+            print >>html, open(dashlog).read()
+            print >>html, "</PRE>"
+
+        print >>html, "</TD></TR></TABLE>"
+        print >>html, "</HTML>"
+        html.close()
+    
+    def makeRunReport(snippetFile, infoPat, runInfo, configName,
+                      status, absRunDir, relRunDir):
+
+        match = search(infoPat, runInfo)
+        if not match: return
+        runNum = int(match.group(1))
+        year   = int(match.group(2))
+        month  = int(match.group(3))
+        day    = int(match.group(4))
+        hr     = int(match.group(5))
+        mins   = int(match.group(6))
+        sec    = int(match.group(7))
+        dur    = int(match.group(8))
+
+        generateSnippet(snippetFile, runNum, month, day, year, hr, mins, sec, dur,
+                        configName, relRunDir+"/run.html", status)
+        makeSummaryHtml(absRunDir, runNum, month, day, year, hr,
+                        mins, sec, dur, configName, status)
 
     infoPat = r'(\d+)_(\d\d\d\d)(\d\d)(\d\d)_(\d\d)(\d\d)(\d\d)_(\d+)'
 
@@ -86,6 +157,9 @@ if __name__ == "__main__":
             ia = int(amatch.group(n)); ib = int(bmatch.group(n))
             if ia != ib: return ib-ia
         return 0
+
+    def getSnippetHtml(lines):
+        return open(snippetFile).read()
 
     allSummaryHtml = runDir + "/index.html"
     allSummaryFile = open(allSummaryHtml, "w")
@@ -103,12 +177,13 @@ if __name__ == "__main__":
     </tr>
     """
 
-    l =  list(listdir(opt.spadeDir))
+    l = listdir(opt.spadeDir)
     l.sort(cmp)
     for f in l:
         prefix = 'SPS-pDAQ-run-'
         if search(r'.done$', f): continue # Skip SPADE .done semaphores
-        match = search(r'%s(\S+)\.' % prefix, f)
+        if search(r'.sem$', f): continue # Skip SPADE .done semaphores
+        match = search(r'%s(\S+?)\.' % prefix, f)
         if match:
             runInfoString = match.group(1)
             match = search(infoPat, runInfoString)
@@ -119,12 +194,13 @@ if __name__ == "__main__":
             tarFile     = opt.spadeDir + "/" + f
             copyFile    = outDir + "/" + f
             datTar      = outDir + "/" + prefix + runInfoString + ".dat.tar"
-            snippetFile = outDir + "/.snippet.html"
-            linkDir     = runInfoString + "/" 
+            snippetFile = outDir + "/.snippet"
+            linkDir     = runInfoString + "/"
             # print datTar
             # Skip if tarball has already been copied
             if not exists(copyFile) or not exists(snippetFile) \
-               or not exists(datTar) or opt.replaceAll:
+               or not exists(datTar) \
+               or opt.replaceAll:
                 print "%s -> %s/" % (f, outDir)
 
                 # Move tarballs into target run directories
@@ -158,17 +234,17 @@ if __name__ == "__main__":
                         if s: configName = s.group(1)
 
                     # Remember more precise unpacked location for link
-                    if search(r'daqrun(\d+)/$', el): 
+                    if search(r'(daqrun\d+)/$', el): 
                         linkDir = runInfoString + "/" + el
-                        
+
                 tar.close()
 
                 # Make HTML snippet for run summaries
-                makeSnippet(snippetFile, infoPat, runInfoString, linkDir, configName, status)
+                makeRunReport(snippetFile, infoPat, runInfoString, 
+                              configName, status, runDir+"/"+linkDir,
+                              linkDir)
 
-
-            lines = open(snippetFile).read()
-            print >>allSummaryFile, lines
+            print >>allSummaryFile, getSnippetHtml(snippetFile)
             
     print >>allSummaryFile, """
     </table>
