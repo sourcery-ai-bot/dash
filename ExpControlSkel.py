1d390fb8134fbabb7f9d160b312efbef0ce321c8
John Jacobsen <john@mail.npxdesigns.com>
2007-02-10 08:22:10 +0000

    dash - ExpControlSkel.py - remember run numbers; ALSO: improve exception handling to make it more resilient to various exceptions

--- ExpControlSkel.py
+++ ExpControlSkel.py
@@ -7,6 +7,8 @@ Started November, 2006
 """
 
 from DAQRunIface import DAQRunIface
+from os.path import join
+from os import environ
 from datetime import *
 from sys import argv
 import optparse
@@ -18,23 +20,49 @@ def updateStatus(oldStatus, newStatus):
         print "%s: %s -> %s" % (datetime.now(), oldStatus, newStatus)
     return newStatus
 
+def setLastRunNum(runFile, runNum):
+    fd = open(runFile, 'w')
+    print >>fd, runNum
+    fd.close()
+    
+def getLastRunNum(runFile):
+    try:
+        f = open(runFile, "r")
+        ret = f.readline()
+        f.close()
+        return int(ret.rstrip('\r\n'))
+    except:
+        return None
+
 def main():
     "Main program"
     p = optparse.OptionParser()
+    p.add_option("-c", "--config-name", action="store", type="string",      dest="configName")
+    p.add_option("-d", "--duration-seconds", action="store", type="int",    dest="duration")
+    p.add_option("-n", "--num-runs",         action="store", type="int",    dest="numRuns")
     p.add_option("-p", "--remote-port",      action="store", type="int",    dest="portNum")
     p.add_option("-r", "--remote-node",      action="store", type="string", dest="nodeName")
-    p.add_option("-n", "--num-runs",         action="store", type="int",    dest="numRuns")
-    p.add_option("-d", "--duration-seconds", action="store", type="int",    dest="duration")
+    p.add_option("-s", "--starting-run",     action="store", type="int",    dest="startRunNum",
+                 help="Run number to start with")
+    
     p.add_option("-x", "--show-status-xml",  action="store_true",           dest="showXML")
-    p.add_option("-c", "--config-name", action="store", type="string",      dest="configName")
-    p.set_defaults(nodeName   = "localhost",
-                   numRuns    = 10000000,
-                   portNum    = 9000,
-                   duration   = 300,
-                   showXML    = False,
-                   configName = "hub1001sim")
+    p.set_defaults(nodeName    = "localhost",
+                   numRuns     = 10000000,
+                   portNum     = 9000,
+                   duration    = 300,
+                   showXML     = False,
+                   startRunNum = None,
+                   configName  = "hub1001sim")
     opt, args = p.parse_args()
 
+    runFile = join(environ[ "HOME" ], ".last_pdaq_run")
+    
+    startRunNum = 1
+    lastRunNum = getLastRunNum(runFile)
+    if lastRunNum != None: startRunNum = lastRunNum + 1
+    if opt.startRunNum: startRunNum = opt.startRunNum
+
+    if startRunNum < 1: raise Exception("Starting run number must be > 0, got %s!" % startRunNum);
     
     # Connect to DAQ run server
     daqiface     = DAQRunIface(opt.nodeName, opt.portNum)
@@ -44,56 +72,104 @@ def main():
     sleeptime    = 0.4
     xmlIval      = 10
     lastState    = None
+
     try:
-        for runNumber in xrange(1, opt.numRuns+1):
+        for runNumber in xrange(startRunNum, opt.numRuns+startRunNum):
             # Start run
             print "Starting run %d" % runNumber
-            daqiface.start(runNumber, configName)
-            while 1:
-                status = daqiface.getState()
-                lastState = updateStatus(lastState, status)
-                if status == "ERROR" :
-                    daqiface.recover()
-                if status == "RUNNING" or status == "STOPPED" : break
-                time.sleep(sleeptime)
+            setLastRunNum(runFile, runNumber)
+            try:
+                daqiface.start(runNumber, configName)
+                while 1:
+                    status = daqiface.getState()
+                    lastState = updateStatus(lastState, status)
+                    if status == "ERROR" : daqiface.recover()
+                    if status == "RUNNING" or status == "STOPPED" : break
+                    time.sleep(sleeptime)
+            except KeyboardInterrupt, k:
+                print "\nKeyboard interrupt before start."
+                raise SystemExit
+            except Exception, e:
+                print "Run start failed: %s" % e
+                daqiface.recover() # If recovery throws an exception then we're hosed
+                continue
 
             if status == "STOPPED": continue # Restart if we had an error
-            
+
             # Monitor run
-            tstart = datetime.now()
-            txml   = None
+            tstart   = datetime.now()
+            txml     = None
+            hadError = False
             while True:
                 tnow = datetime.now()
                 if tnow-tstart > timedelta(seconds=opt.duration): break
-                status = daqiface.getState()
+                try:
+                    status = daqiface.getState()
+                except KeyboardInterrupt, k: raise
+                except Exception, e:
+                    print "Get status failed: %s" % e
+                    daqiface.recover()
+                    hadError = True
+                    break
+
                 lastState = updateStatus(lastState, status)
                 if(status == "ERROR"): break
-                if opt.showXML:
-                    if not txml or tnow-txml > timedelta(seconds=xmlIval):
+                if opt.showXML and (not txml or tnow-txml > timedelta(seconds=xmlIval)):
+                    try:
                         print daqiface.getSummary()
-                        txml = tnow
+                    except KeyboardInterrupt, k: raise
+                    except Exception, e:
+                        print "getSummary failed: %s" % e
+                        daqiface.recover()
+                        hadError = True
+                        break
+                    txml = tnow
+
                 time.sleep(sleeptime)
 
+            if hadError: continue # Restart run if something failed
+
             # Stop run, do error recovery if needed
-            status = daqiface.getState()
+            try:
+                status = daqiface.getState()
+            except KeyboardInterrupt, k: raise
+            except Exception, e:
+                print "Get status failed: %s" % e
+                daqiface.recover()
+                continue
+
             lastState = updateStatus(lastState, status)
             if(status == "ERROR"):
                 daqiface.recover()
+                continue
             else:
-                daqiface.stop()
-                
+                try:
+                    daqiface.stop()
+                except KeyboardInterrupt, k: raise
+                except Exception, e:
+                    print "Stop operation failed: %s" % e
+                    daqiface.recover()
+                    continue
+
             while 1:
-                status = daqiface.getState()
+                try:
+                    status = daqiface.getState()
+                except KeyboardInterrupt, k: raise
+                except Exception, e:
+                    print "Get state failed: %s" % e
+                    daqiface.recover()
+                    continue
                 lastState = updateStatus(lastState, status)
                 if status == "STOPPED": break
-                if status == "ERROR" : daqiface.recover()
-                time.sleep(sleeptime)
-        
-    except Exception, e:
-        daqiface.stop()
-        print e
-        raise SystemExit
+                if status == "ERROR" :
+                    daqiface.recover()
+                    continue
 
+                time.sleep(sleeptime)
+    except KeyboardInterrupt, k:
+        daqiface.stop()
+        raise SystemExit
+    
     daqiface.release()
     
 if __name__ == "__main__": main()
