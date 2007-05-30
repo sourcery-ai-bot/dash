adccecdbd3f2c6cfe24d5cf3025af45df3081b73
John Jacobsen <john@mail.npxdesigns.com>
2007-05-30 20:43:32 +0000

    dash - Make scripts to verify that all DOMs are in correct state (powered on and in iceboot)

---
+++ DAQHub.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# John Jacobsen
+# john@mail.npxdesigns.com
+# NPX Designs, Inc. for UW-IceCube
+
+class DAQHub:
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__": main()
+
