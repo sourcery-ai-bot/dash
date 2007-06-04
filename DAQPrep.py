e3cc17c4af4cd1cb2d936616a6ac75b687158e5b
John Jacobsen <john@mail.npxdesigns.com>
2007-06-04 16:52:41 +0000

    dash - Make scripts to verify that all DOMs are in correct state (powered on and in iceboot)

--- DAQPrep.py
+++ DAQPrep.py
@@ -76,7 +76,7 @@ def main():
     cmds.wait()
     
     for hub in hublist:
-        print "Hub %d:"
+        print "Hub %d:" % hub
         print cmds.getResult[ids[hub]]
         
 if __name__ == "__main__": main()
