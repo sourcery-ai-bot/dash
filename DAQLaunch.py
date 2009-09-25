7a1af792dafe7f106a39bd43471cba335a1b94fc
Keith Beattie <ksbeattie@lbl.gov>
2009-09-25 23:15:00 +0000

    DAQLaunch has IBM-specific JVM args (though not really related, just discovered during that work) - Removed duplicate setting of metaDir var.

--- DAQLaunch.py
+++ DAQLaunch.py
@@ -58,14 +58,7 @@ else:
 sys.path.append(join(metaDir, 'src', 'main', 'python'))
 from SVNVersionInfo import get_version_info
 
-SVN_ID = "$Id: DAQLaunch.py 4572 2009-08-28 21:18:10Z dglo $"
-
-# Find install location via $PDAQ_HOME, otherwise use locate_pdaq.py
-if environ.has_key("PDAQ_HOME"):
-    metaDir = environ["PDAQ_HOME"]
-else:
-    from locate_pdaq import find_pdaq_trunk
-    metaDir = find_pdaq_trunk()
+SVN_ID = "$Id: DAQLaunch.py 4606 2009-09-25 23:15:00Z ksb $"
 
 class HostNotFoundForComponent   (Exception): pass
 class ComponentNotFoundInDatabase(Exception): pass
