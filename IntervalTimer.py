cea52d7ce6a2f22a7f371bc86df8aa4935dcdcee
Dave Glowacki <dglo@icecube.wisc.edu>
2009-09-28 17:46:29 +0000

    Minor cleanup

--- IntervalTimer.py
+++ IntervalTimer.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
 
-import datetime
+from datetime import datetime, timedelta
 
 class IntervalTimer(object):
     """
-    Timer which triggers each time the specified interval has passed.
+    Timer which triggers each time the specified number of seconds has passed.
     """
     def __init__(self, interval):
         self.__isTime = False
-        self.__tlast = None
+        self.__nextTime = None
         self.__interval = interval
 
     def isTime(self):
         "Return True if another interval has passed"
         if not self.__isTime:
-            now = datetime.datetime.now()
+            now = datetime.now()
 
-            if not self.__tlast:
+            if not self.__nextTime:
+                self.__nextTime = now
                 self.__isTime = True
             else:
-                dt  = now - self.__tlast
+                dt  = now - self.__nextTime
 
                 if dt.seconds + (dt.microseconds * 0.000001) > self.__interval:
                     self.__isTime = True
@@ -27,5 +28,6 @@ class IntervalTimer(object):
         return self.__isTime
 
     def reset(self):
-        self.__tlast = datetime.datetime.now()
+        "Reset timer for the next interval"
+        self.__nextTime = datetime.now()
         self.__isTime = False
