4bda06221f974b879da5c81af488ce9a7dcd1958
Dave Glowacki <dglo@icecube.wisc.edu>
2010-09-09 17:12:34 +0000

    Finish enabling rpc_runset_count

--- CnCServer.py
+++ CnCServer.py
@@ -30,7 +30,7 @@ else:
 sys.path.append(os.path.join(metaDir, 'src', 'main', 'python'))
 from SVNVersionInfo import get_version_info
 
-SVN_ID  = "$Id: CnCServer.py 5168 2010-09-08 20:13:57Z dglo $"
+SVN_ID  = "$Id: CnCServer.py 5182 2010-09-09 17:12:34Z dglo $"
 
 class CnCServerException(Exception): pass
 
@@ -551,8 +551,8 @@ class CnCServer(DAQPool):
             self.__server.register_function(self.rpc_ping)
             self.__server.register_function(self.rpc_runset_active)
             self.__server.register_function(self.rpc_runset_break)
-            self.__server.register_function(self.rpc_runset_break)
             self.__server.register_function(self.rpc_runset_configname)
+            self.__server.register_function(self.rpc_runset_count)
             self.__server.register_function(self.rpc_runset_debug)
             self.__server.register_function(self.rpc_runset_events)
             self.__server.register_function(self.rpc_runset_list)
